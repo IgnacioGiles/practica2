a174ef0bd608c812639cf38ccd642d0aad2411c3
unknown <ignaciogilesarauz@gmail.com>
2025-03-17 18:30:40 -0400

    guardado de proy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+import { ComponentsComponent } from './components/components.component';
+import { ServicesComponent } from './services/services.component';
+import { ModelsComponent } from './models/models.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,   
+    ComponentsComponent,
+    ServicesComponent,
+    ModelsComponent,
+    NavbarComponent,
+    FooterComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
